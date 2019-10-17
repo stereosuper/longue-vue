318954a58d778725b514d9ca5784fdf1c55479fd
Alphability <albanmezino@gmail.com>
2019-10-17 17:54:04 +0200

    ✨ store using vuex modules with optional i18n

--- template/nuxt/plugins/globals.js
+++ template/nuxt/plugins/globals.js
@@ -1,4 +1,8 @@
 import Vue from 'vue';
+<%_ if (stereorepoSac) { _%>
+// NOTE: Uncomment this line to import superComponents
+// import { useSacVue, superWindowVue } from '@stereorepo/sac';
+<%_ } _%>
 
 Vue.prototype.$stereosuper = {
     ...Vue.prototype.$stereosuper,
@@ -9,5 +13,10 @@ const isDevEnv = process.env.NODE_ENV !== 'production';
 Vue.config.performance = isDevEnv;
 
 // Set Vue.use here
+<%_ if (stereorepoSac) { _%>
+// NOTE: Uncomment these lines to use superComponents like superWindowVue
+// Vue.use(useSacVue);
+// Vue.use(superWindowVue);
+<%_ } _%>
 
 // Set Vue.component here
---
+++ template/nuxt/store/index.js
@@ -0,0 +1,23 @@
+<%_ if (i18n) { _%>
+export const state = () => ({
+    localePath: '/',
+});
+<%_ } else { _%>
+// export const state = () => ({});
+<%_ } _%>
+
+// export const getters = () => {};
+
+
+<%_ if (i18n) { _%>
+export const mutations = {
+    setLocalePath(state, { locale, defaultLocale }) {
+        state.localePath = `${defaultLocale !== locale ? `/${locale}/` : '/'}`;
+    },
+};
+<%_ } else { _%>
+// export const mutations = {};
+<%_ } _%>
+
+
+// export const actions = {};
