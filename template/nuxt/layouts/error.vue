c36f0e422da022c2cb944b3b37c05324cd9f1430
Alphability <albanmezino@gmail.com>
2019-11-10 04:37:59 +0100

    ✨ error layout

---
+++ template/nuxt/layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="error container">
+        <div class="error-text">
+            <header class="error-header">
+                <h1 class="h1">
+                    {{ error.statusCode }}
+                </h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        error: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
