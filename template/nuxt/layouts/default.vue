d825bb065b04d2f442e8dc645102b4835c6e5d3a
Alphability <albanmezino@gmail.com>
2019-11-10 22:24:41 +0100

    🐛 scss variables corrections + superWindow component init

--- template/nuxt/layouts/default.vue
+++ template/nuxt/layouts/default.vue
@@ -4,4 +4,17 @@
     </div>
 </template>
 
+<script>
+export default {
+    mounted() {
+        <%_ if (stereorepoSac) { _%>
+        this.$stereorepo.superWindow.initializeWindow(this.$store);
+        <%_ } _%>
+        this.$nextTick(() => {
+            this.$store.commit('setLoading', false);
+        });
+    }
+};
+</script>
+
 <style></style>
--- template/nuxt/plugins/globals.js
+++ template/nuxt/plugins/globals.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 <%_ if (stereorepoSac) { _%>
-// NOTE: Uncomment this line to import superComponents
-// import { useSacVue, superWindowVue } from '@stereorepo/sac';
+// NOTE: Here's how to import superComponents
+import { useSacVue, superWindowVue } from '@stereorepo/sac';
 <%_ } _%>
 
 Vue.prototype.$stereosuper = {
@@ -14,9 +14,9 @@ Vue.config.performance = isDevEnv;
 
 // Set Vue.use here
 <%_ if (stereorepoSac) { _%>
-// NOTE: Uncomment these lines to use superComponents like superWindowVue
-// Vue.use(useSacVue);
-// Vue.use(superWindowVue);
+// NOTE: Here's an example of how to use superComponents like superWindowVue
+Vue.use(useSacVue);
+Vue.use(superWindowVue);
 <%_ } _%>
 
 // Set Vue.component here
