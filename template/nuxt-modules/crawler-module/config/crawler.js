7415267ef1674fc10daee373d289f4757b07447b
Alphability <albanmezino@gmail.com>
2020-02-17 11:27:55 +0100

    🎨 crawler query example

--- template/nuxt-modules/crawler-module/config/crawler.js
+++ template/nuxt-modules/crawler-module/config/crawler.js
@@ -1,10 +1,22 @@
 import gql from 'graphql-tag';
 
+/**
+ * NOTE: You can get the slugs available for generation from here
+ * Your query will resemble something like the one below 👇
+ * To avoid apollo errors from an empty CMS we commented it.
+ */
+
+// query getAllSlugs($lang: SiteLocale) {
+//     allBasicPages(locale: $lang) {
+//         slug
+//         _modelApiKey
+//     }
+// }
+
 export default gql`
-    query getAllSlugs($lang: SiteLocale) {
-        allBasicPages(locale: $lang) {
-            slug
-            _modelApiKey
+    query Redirections {
+        _site {
+            __typename
         }
     }
 `;
