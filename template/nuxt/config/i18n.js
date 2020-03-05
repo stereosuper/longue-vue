60758ea070728ef87b7c7b3e25fc048629ace2cc
Alphability <albanmezino@gmail.com>
2020-03-05 12:16:17 +0100

    💡i18n important note default locale

--- template/nuxt/config/i18n.js
+++ template/nuxt/config/i18n.js
@@ -2,6 +2,10 @@ import { routes } from '../assets/js/constants/routes';
 
 export const defaultLocale = 'fr';
 
+/**
+ * NOTE: Make sure that default locale is the last one!
+ * SEE: https://nuxt-community.github.io/nuxt-i18n/routing.html#strategy
+ */
 export const locales = [{ code: 'fr', iso: 'fr_FR' }];
 
 // 🚦 Specific routes
